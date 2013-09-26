55f665b7f1bb26e34665143e8bba9f361ecc607c
media tech <andrae@iMac2.local>
2013-09-26 11:55:01 -0500

    Initial Commit

---
+++ Project_Cars/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project_Cars
+//
+//  Created by media tech on 9/26/13.
+//  Copyright (c) 2013 com.mediatech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Project_CarsTests/Project_CarsTests.m
@@ -0,0 +1,34 @@
+//
+//  Project_CarsTests.m
+//  Project_CarsTests
+//
+//  Created by media tech on 9/26/13.
+//  Copyright (c) 2013 com.mediatech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Project_CarsTests : XCTestCase
+
+@end
+
+@implementation Project_CarsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
